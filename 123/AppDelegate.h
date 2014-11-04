cbd40ce1195ff09f8bf2cc0a171987242f0ee4d2
ZLMac <sweetgirl@zlMac.local>
2014-11-04 14:49:28 +0800

    Initial Commit

---
+++ 123/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  123
+//
+//  Created by ZLMac on 14-11-4.
+//  Copyright (c) 2014年 lgwh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
